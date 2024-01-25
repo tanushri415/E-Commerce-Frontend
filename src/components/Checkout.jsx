dd9ecad04d1745bbec438de91cd599ac553ffb33
Tanushri Jain <jaintanushri34@gmail.com>
2024-01-25 00:21:42 -0600

    checkout page, styling for shopping cart

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
 import { Box } from '@mui/system';
 import Header from './Header';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  InputLabel,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import OrderSummary from './OrderSummary';
+import SupportedPayments from './SupportedPayments';
+import MuiPhoneNumber from 'mui-phone-number';
+import './Checkout.css';
 const Checkout = () => {
   const [successMessage, setSuccessMessage] = useState('');
 
   const handleSubmit = (e) => {
-    e.prevent.default();
+    e.preventDefault();
+    console.log('hurray');
     setSuccessMessage('Order Placed Successfully!');
   };
-
   return (
     <Box
       sx={{
@@ -17,45 +31,234 @@ const Checkout = () => {
         overflow: 'auto',
       }}>
       <Header />
-      <Box sx={{ marginTop: '100px' }}>
-        <h3>Checkout Summary</h3>
-        <form>
-          <Box>
-            <h4>Personal Information</h4>
 
-            <label>
-              Full Name: <input type='text'></input>
-            </label>
-            <label>
-              Phone: <input type='number'></input>
-            </label>
-            <label>
-              Card details:<input></input>
-            </label>
+      <Box
+        sx={{
+          backgroundColor: '#EAEDED',
+          marginLeft: '50px',
+          marginRight: '50px',
+          marginBottom: '15px',
+          marginTop: '100px',
+        }}>
+        <Typography variant='h4' sx={{ textAlign: 'center', color: 'blue' }}>
+          Checkout Summary
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              borderRadius: '9px',
+              borderColor: `rgb(221,221,221)`,
+              gap: '15px',
+              width: '50%',
+            }}>
+            <Card>
+              <CardHeader title='Personal Information' />
+              <CardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderRadius: '9px',
+                  borderColor: `rgb(221,221,221)`,
+                  marginRight: '50px',
+                  marginLeft: '50px',
+                }}>
+                <table className='table'>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='fullname'>
+                        Full Name
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='fullname'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='phone'>
+                        Phone
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <MuiPhoneNumber
+                        defaultCountry={'us'}
+                        id='phone'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        // onChange={handleOnChange}
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='carddetails'>
+                        Card Details
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='carddetails'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                </table>
+
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    gap: '10px',
+                  }}></Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'baseline',
+                    gap: '10px',
+                  }}></Box>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader title='Shipping Address' />
+              <CardContent
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderRadius: '9px',
+                  borderColor: `rgb(221,221,221)`,
+                  marginRight: '50px',
+                  marginLeft: '50px',
+                }}>
+                <table className='table'>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='streetaddress'>
+                        Street Address
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='streetaddress'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='city'>
+                        City
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='city'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='state'>
+                        State
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='state'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td width='25%'>
+                      <InputLabel
+                        sx={{
+                          fontWeight: 'bold',
+                        }}
+                        htmlFor='pincode'>
+                        Pincode
+                      </InputLabel>
+                    </td>
+                    <td width='75%'>
+                      <TextField
+                        required
+                        id='pincode'
+                        size='small'
+                        variant='outlined'
+                        sx={{ fontSize: '13px', width: '100%' }}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                </table>
+              </CardContent>
+            </Card>
+            <Button
+              color='gold'
+              variant='contained'
+              size='medium'
+              type='submit'>
+              Place Order
+            </Button>
+            <Typography color='green'>{successMessage}</Typography>
           </Box>
-          <Box>
-            <h4>Shipping Address</h4>
-            <label>
-              Street Address:
-              <input type='text'></input>
-            </label>
-            <label>
-              City:
-              <input type='text'></input>
-            </label>
-            <label>
-              State:
-              <input type='text'></input>
-            </label>
-            <label>
-              Pincode:
-              <input type='text'></input>
-            </label>
-          </Box>
-          <button type='submit' onClick={() => handleSubmit}>
-            Place Order
-          </button>
-        </form>
+          <Stack sx={{ gap: '15px', width: '33.34%', maxWidth: '450px' }}>
+            <OrderSummary showChkoutBtn={false} />
+            <SupportedPayments />
+          </Stack>
+        </Box>
       </Box>
     </Box>
   );
