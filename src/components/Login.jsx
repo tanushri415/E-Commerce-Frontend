26e9828520925a46b432927c5e67baa354321d95
Tanushri Jain <jaintanushri34@gmail.com>
2024-01-25 14:12:22 -0600

    including header in login/register page, psuedo code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 import { userApi } from '../api';
 import { useNavigate } from 'react-router-dom';
+import Header from './Header';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -88,125 +89,138 @@ const Login = () => {
   };
 
   return (
-    <Container
-      maxWidth='xs'
+    <Box
       sx={{
         display: 'flex',
-        flexDirection: 'column',        
+        flexDirection: 'column',
+        overflow: 'auto',
       }}>
-      <Stack spacing={2} alignItems={'center'} pt={'14px'}>
-        <Typography
-          sx={{
-            width: '103px',
-            height: '31px',
-            backgroundPositionX: '-5px',
-            backgroundPositionY: '-130px',
-            backgroundImage: `url("https://m.media-amazon.com/images/S/sash/mPGmT0r6IeTyIee.png")`,
-          }}
-        />
-        <Box
-          component='form'
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            borderWidth: '3px',
-            borderStyle: 'solid',
-            borderRadius: '9px',
-            borderColor: `rgb(221,221,221)`,
-            paddingTop: '10px',
-            paddingLeft: '24px',
-            paddingBottom: '24px',
-            paddingRight: '24px',
-            width: '100%',
-          }}>
+      <Header />
+      <Container
+        maxWidth='xs'
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          marginTop: '100px',
+        }}>
+        <Stack spacing={2} alignItems={'center'} pt={'14px'}>
+          <Typography
+            sx={{
+              width: '103px',
+              height: '31px',
+              backgroundPositionX: '-5px',
+              backgroundPositionY: '-130px',
+              backgroundImage: `url("https://m.media-amazon.com/images/S/sash/mPGmT0r6IeTyIee.png")`,
+            }}
+          />
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              borderWidth: '3px',
+              borderStyle: 'solid',
+              borderRadius: '9px',
+              borderColor: `rgb(221,221,221)`,
+              paddingTop: '10px',
+              paddingLeft: '24px',
+              paddingBottom: '24px',
+              paddingRight: '24px',
+              width: '100%',
+            }}>
+            <Typography
+              variant='h5'
+              sx={{ textAlign: 'center', marginBottom: '10px' }}>
+              Login to your Account
+            </Typography>
+            <InputLabel
+              sx={{
+                marginBottom: '1px',
+                color: 'black',
+                fontWeight: '700',
+                fontSize: '13px',
+              }}
+              htmlFor='username'>
+              User Name
+            </InputLabel>
+            <TextField
+              id='username'
+              name='username'
+              required
+              // label='Username'
+              size='small'
+              variant='outlined'
+              placeholder='User Name'
+              sx={{ marginBottom: '10px', fontSize: '13px' }}
+              autoComplete='off'
+              value={formValues.username.value}
+              error={formValues.username.error}
+              onChange={handleFormElementChange}
+              helperText={
+                formValues.username.error && formValues.username.errorMessage
+              }
+            />
+            <InputLabel
+              sx={{
+                marginBottom: '1px',
+                color: 'black',
+                fontWeight: '700',
+                fontSize: '13px',
+              }}
+              htmlFor='password'>
+              Password
+            </InputLabel>
+            <TextField
+              id='password'
+              name='password'
+              required
+              size='small'
+              variant='outlined'
+              type='password'
+              placeholder='Password'
+              sx={{ marginBottom: '10px' }}
+              // inputProps={{ classes: { input: { fontSize: 13 } } }}
+              value={formValues.password.value}
+              error={formValues.password.error}
+              onChange={handleFormElementChange}
+              helperText={
+                formValues.password.error && formValues.password.errorMessage
+              }
+            />
+            <Typography
+              sx={{
+                textAlign: 'left',
+                marginBottom: '10px',
+                fontSize: '10px',
+                color: 'red',
+              }}>
+              {loginError}
+            </Typography>
+            <Button
+              color='gold'
+              variant='contained'
+              size='medium'
+              type='submit'>
+              Login
+            </Button>
+          </Box>
           <Typography
             variant='h5'
             sx={{ textAlign: 'center', marginBottom: '10px' }}>
-            Login to your Account
+            New to Amazon?
           </Typography>
-          <InputLabel
-            sx={{
-              marginBottom: '1px',
-              color: 'black',
-              fontWeight: '700',
-              fontSize: '13px',
-            }}
-            htmlFor='username'>
-            User Name
-          </InputLabel>
-          <TextField
-            id='username'
-            name='username'
-            required
-            // label='Username'
-            size='small'
-            variant='outlined'
-            placeholder='User Name'
-            sx={{ marginBottom: '10px', fontSize: '13px' }}
-            autoComplete='off'
-            value={formValues.username.value}
-            error={formValues.username.error}
-            onChange={handleFormElementChange}
-            helperText={
-              formValues.username.error && formValues.username.errorMessage
-            }
-          />
-          <InputLabel
-            sx={{
-              marginBottom: '1px',
-              color: 'black',
-              fontWeight: '700',
-              fontSize: '13px',
-            }}
-            htmlFor='password'>
-            Password
-          </InputLabel>
-          <TextField
-            id='password'
-            name='password'
-            required
-            size='small'
-            variant='outlined'
-            type='password'
-            placeholder='Password'
-            sx={{ marginBottom: '10px' }}
-            // inputProps={{ classes: { input: { fontSize: 13 } } }}
-            value={formValues.password.value}
-            error={formValues.password.error}
-            onChange={handleFormElementChange}
-            helperText={
-              formValues.password.error && formValues.password.errorMessage
-            }
-          />
-          <Typography
-            sx={{
-              textAlign: 'left',
-              marginBottom: '10px',
-              fontSize: '10px',
-              color: 'red',
-            }}>
-            {loginError}
-          </Typography>
-          <Button color='gold' variant='contained' size='medium' type='submit'>
-            Login
+          <Button
+            color='gold'
+            variant='contained'
+            size='large'
+            onClick={handleRegisterAccount}>
+            Create your Account
           </Button>
-        </Box>
-        <Typography
-          variant='h5'
-          sx={{ textAlign: 'center', marginBottom: '10px' }}>
-          New to Amazon?
-        </Typography>
-        <Button
-          color='gold'
-          variant='contained'
-          size='large'
-          onClick={handleRegisterAccount}>
-          Create your Account
-        </Button>
-      </Stack>
-    </Container>
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 export default Login;
