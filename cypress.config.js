55389e1287332cb15bfe265ca9dc0c8505242963
Tanushri Jain <jaintanushri34@gmail.com>
2024-03-22 22:26:08 -0500

    new install's

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+import path from 'path';
+import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor());
+    },
+  },
+});
